c74af7c35cb0a82805685bac1e01eab61b90635a
Lina <lina.fakhrutdinova@gmail.com>
2021-03-14 17:53:51 +0300

    added validation

--- js/form.js
+++ js/form.js
@@ -1,11 +1,17 @@
 import {ACCURACY} from './data.js'
-// inactive state of the card
+
+const MAX_INPUT_PRICE = 1000000;
 
 const form = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 const formElement = form.querySelectorAll('fieldset');
 const mapFiltersElement = mapFilters.querySelectorAll('select, fieldset');
 const address = form.querySelector('#address');
+const titleInput = form.querySelector('#title');
+const houseInputPrice = form.querySelector('#price');
+const roomsNumber = form.querySelector('#room_number');
+const guestsNumber = form.querySelector('#capacity');
+
 
 const fillAddress = function ({lat, lng}) {
   const lattitude = lat.toFixed(ACCURACY);
@@ -14,6 +20,7 @@ const fillAddress = function ({lat, lng}) {
   address.value = `${lattitude} ${longitude}`;
 }
 
+// inactive state of the card
 let className = null;
 const changeClass = function () {
   formElement ? className = 'ad-form--disabled' : className = 'map__filters--disabled';
@@ -44,6 +51,45 @@ const enableForm = function () {
   activateForm(mapFilters, mapFiltersElement);
   activateForm(form, formElement);
 }
+
+// validate title field
+
+titleInput.addEventListener('invalid', function() {
+  if (titleInput.validity.tooshort) {
+    titleInput.setCustomValidity('Заголовок должен состоять минимум из 30 символов');
+  } else if (titleInput.validity.tooLong) {
+    titleInput.setCustomValidity('Заголовок должен состоять максимум из 100 символов');
+  } else if (titleInput.validity.valueMissing) {
+    titleInput.setCustomValidity('Необходимо заполнить поле');
+  } else {
+    titleInput.setCustomValidity('');
+  }
+})
+
+//validate price field
+houseInputPrice.addEventListener('input', function() {
+  const valuePrice = houseInputPrice.value;
+  if (valuePrice > MAX_INPUT_PRICE) {
+    houseInputPrice.setCustomValidity('Превышено допустимое значение цены');
+  } else {
+    houseInputPrice.setCustomValidity('');
+  }
+})
+
+//sync rooms and guests number
+const setLimitGuests = function() {
+  if (Number.parseInt(roomsNumber.value) < Number.parseInt(guestsNumber.value)) {
+    guestsNumber.setCustomValidity('Число гостей не должно превышать число комнат');
+  } else if (roomsNumber.value !== '100' && guestsNumber.value === '0') {
+    guestsNumber.setCustomValidity('Вариант не для гостей подходит только для 100 комнат');
+  } else {
+    guestsNumber.setCustomValidity('');
+  }
+}
+
+roomsNumber.addEventListener('change', function() {
+  setLimitGuests();
+});
 
 export {
   disableForm,
